ddfebe0e474a0721fc5ca63cbdbb38754541785f
tamurakeito <kit_tamtam@icloud.com>
2025-01-15 15:32:24 +0900

    App Router採用 /appはルーティングメインにしてUIは/componentsに実装

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,5 @@
+import { SignIn } from "@/components/pages/sign-in";
+
+export default function SignInPage() {
+  return <SignIn />;
+}
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import { SignUp } from "@/components/pages/sign-up";
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
